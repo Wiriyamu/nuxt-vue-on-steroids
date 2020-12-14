2ceca8481b9ea2eb6de9c63e0fae5cd69f27289f
Willian Silva <will.devfrontend@gmail.com>
2020-12-14 12:38:48 -0300

    Adding links & navigating around

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>The users View</h1>
+  <div>
+    <h1>The users View</h1>
+    <input
+      v-model.number="userId"
+      style="border: 1px solid black"
+      type="text"
+    />
+    <button
+      style="padding: 5px 10px; background-color: blue; color: white"
+      @click="onLoadUser"
+    >
+      Load User
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Users',
+  data: () => ({
+    userId: null,
+  }),
+
+  methods: {
+    onLoadUser() {
+      this.$router.push(`/users/${this.userId}`)
+    },
+  },
+}
+</script>
