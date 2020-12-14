1b0b7a85f5913966a40e297479d46c910f96a5f5
Willian Silva <will.devfrontend@gmail.com>
2020-12-14 15:28:01 -0300

    Adding a default error page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1>An error occurred, we're sorry!</h1>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: lightcoral;
+  color: white;
+}
+
+h1 {
+  font-size: 24px;
+}
+</style>
