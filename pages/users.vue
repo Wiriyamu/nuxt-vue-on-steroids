0e2cb1a6ab5110e358d1032f4f20fd426a9875fe
Willian Silva <will.devfrontend@gmail.com>
2020-12-14 15:06:12 -0300

    Creating Nested Routes

---
+++ pages/users.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>The users View</h1>
+    <input
+      v-model.number="userId"
+      style="border: 1px solid black"
+      type="text"
+    />
+    <button
+      style="padding: 5px 10px; background-color: blue; color: white"
+      @click="onLoadUser"
+    >
+      Load User
+    </button>
+    <!-- Nested Routes (https://pt.nuxtjs.org/docs/2.x/features/file-system-routing#rotas-aninhadas) -->
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  data: () => ({
+    userId: null,
+  }),
+
+  methods: {
+    onLoadUser() {
+      this.$router.push(`/users/${this.userId}`)
+    },
+  },
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,31 +1,3 @@
 <template>
-  <div>
-    <h1>The users View</h1>
-    <input
-      v-model.number="userId"
-      style="border: 1px solid black"
-      type="text"
-    />
-    <button
-      style="padding: 5px 10px; background-color: blue; color: white"
-      @click="onLoadUser"
-    >
-      Load User
-    </button>
-  </div>
+  <p>Please enter an id and click the button</p>
 </template>
-
-<script>
-export default {
-  name: 'Users',
-  data: () => ({
-    userId: null,
-  }),
-
-  methods: {
-    onLoadUser() {
-      this.$router.push(`/users/${this.userId}`)
-    },
-  },
-}
-</script>
