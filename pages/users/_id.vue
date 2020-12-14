597690292fe739a623cc420d87febc64629de02f
Willian Silva <will.devfrontend@gmail.com>
2020-12-14 14:53:06 -0300

    Validating parameters

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,3 +1,13 @@
 <template>
   <h1>A single user: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  name: 'Id',
+  // Validate Method (https://pt.nuxtjs.org/docs/2.x/components-glossary/pages-validate)
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+}
+</script>
